06881483d822071d0a0113c2b6a293333def36d1
Klaus Landsdorf <klaus@bianco-royal.de>
2019-09-04 15:12:53 +0200

    feat: properties by example of the weather station

--- core/src/protocol/mod.rs
+++ core/src/protocol/mod.rs
@@ -15,6 +15,9 @@ pub mod verb;
 #[cfg(test)]
 mod tests {
     use crate::protocol::object::WoopsaObject;
+    use crate::protocol::property::WoopsaProperty;
+    use crate::protocol::value_type::WoopsaValueType;
+    use std::time::SystemTime;
 
     #[test]
     fn it_works() {
@@ -38,6 +41,49 @@ mod tests {
         println!("/root/objects/ is {}", &objects);
         for (key, value) in &(objects.container.items) {
             println!("item of objects -> {}: {}", key, value);
+        }
+
+        assert_eq!(objects.name(), String::from("Objects"));
+    }
+
+    #[test]
+    fn it_build_weatherstation_model() {
+        let mut root = WoopsaObject::new(String::from("Root"));
+        let mut objects = WoopsaObject::new(String::from("Objects"));
+        
+        let mut weather_station = WoopsaObject::new(String::from("WeatherStation"));
+        weather_station.add_property(WoopsaProperty::new(String::from("Temperature"), String::from("24.2"), WoopsaValueType::Real));
+        weather_station.add_property(WoopsaProperty::new(String::from("IsRaining"), String::from("false"), WoopsaValueType::Logical));
+        weather_station.add_property(WoopsaProperty::new(String::from("Sensitivity"), String::from("0.5"), WoopsaValueType::Real));
+        weather_station.add_property(WoopsaProperty::new(String::from("Altitude"), String::from("430"), WoopsaValueType::Integer));
+        weather_station.add_property(WoopsaProperty::new(String::from("City"), String::from("Geneva"), WoopsaValueType::Text));
+        // Todo: weather_station.add_property(WoopsaProperty::new(String::from("Time"), String::from(SystemTime::now()), WoopsaValueType::DateTime));
+
+        let mut thermostat = WoopsaObject::new(String::from("Thermostat"));
+        thermostat.add_property(WoopsaProperty::new_readonly(String::from("SetPoint"), String::from("24.0"), WoopsaValueType::Real));
+        
+        weather_station.container.insert_item(thermostat);
+        objects.container.insert_item(weather_station);
+        root.container.insert_item(objects);
+
+        println!("/Root/ is {}", &root);
+        for (key, value) in &(root.container.items) {
+            println!("item of root -> {}: {}", key, value);
+        }
+
+        let objects = root.find_item_by_name(String::from("Objects"));
+        println!("/Root/Objects/ is {}", &objects);
+        for (key, value) in &(objects.container.items) {
+            println!("item of objects -> {}: {}", key, value);
+        }
+
+        let weather_station = objects.find_item_by_name(String::from("WeatherStation"));
+        println!("/Root/Objects/WeatherStation/ is {}", &weather_station);
+        for (key, value) in &(weather_station.properties) {
+            println!("property of weather_station -> {}: {}", key, value);
+        }
+        for (key, value) in &(weather_station.container.items) {
+            println!("item of weather_station -> {}: {}", key, value);
         }
 
         assert_eq!(objects.name(), String::from("Objects"));
--- core/src/protocol/property.rs
+++ core/src/protocol/property.rs
@@ -2,6 +2,9 @@ use crate::protocol::element::WoopsaElement;
 use crate::protocol::value::WoopsaValue;
 use crate::protocol::value_type::WoopsaValueType;
 
+use std::fmt;
+use std::time::SystemTime;
+
 pub trait Property {
     fn type_of(&self) -> &'static str;
     fn name(&self) -> String;
@@ -15,7 +18,51 @@ pub struct WoopsaProperty {
     pub readonly: bool,
 }
 
-impl WoopsaProperty {}
+impl WoopsaProperty {
+    pub fn new_readonly(element_name: String, value: String, value_type: WoopsaValueType) -> WoopsaProperty {
+        WoopsaProperty {
+            element: WoopsaElement {
+                name: element_name,
+            },
+            value: WoopsaValue {
+                as_text: value,
+                timestamp: SystemTime::now(),
+                value_type: value_type
+            },
+            value_type: value_type,
+            readonly: true
+        }
+    }
+
+    pub fn new(element_name: String, value: String, value_type: WoopsaValueType) -> WoopsaProperty {
+        WoopsaProperty {
+            element: WoopsaElement {
+                name: element_name,
+            },
+            value: WoopsaValue {
+                as_text: value,
+                timestamp: SystemTime::now(),
+                value_type: value_type
+            },
+            value_type: value_type,
+            readonly: false
+        }
+    }
+
+    pub fn set_value(&mut self, value: String, value_type: WoopsaValueType) {
+        self.value.value_type = value_type;
+        self.value.as_text = value;
+        self.value.timestamp = SystemTime::now();
+    }
+
+    pub fn set_readonly(&mut self) {
+        self.readonly = true;
+    }
+
+    pub fn set_read_write(&mut self) {
+        self.readonly = false;
+    }
+}
 
 impl Property for WoopsaProperty {
     fn type_of(&self) -> &'static str {
@@ -28,5 +75,13 @@ impl Property for WoopsaProperty {
     
     fn is_read_only(&self) -> bool {
         return self.readonly.clone();
+    }
+}
+
+impl fmt::Display for WoopsaProperty {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({} with value {})", 
+        self.element.name,
+        self.value.as_text)
     }
 }
